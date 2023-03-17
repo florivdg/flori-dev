2e62ebf390156a706a0b21f3013614763c169124
Florian van der Galiën <hallo@flori.dev>
2023-03-17 10:37:49 +0100

    Fixed safari clip-text issues

--- src/components/CurrentBrowser.vue
+++ src/components/CurrentBrowser.vue
@@ -3,11 +3,11 @@
     <div
       class="flex -rotate-1 -skew-x-3 flex-col gap-4 rounded-lg border border-slate-300 bg-gray-100 p-4 shadow-xl @4xl:flex-row @4xl:items-center @4xl:justify-center dark:border-slate-700 dark:bg-slate-900"
     >
-      <h3
-        class="self-center bg-gradient-to-tr from-pink-500 to-sky-500 bg-clip-text px-2 text-6xl font-black text-transparent @xl:self-start @4xl:self-auto @4xl:text-5xl @5xl:text-7xl"
+      <p
+        class="fix-bg-clip-text-safari self-center bg-gradient-to-tr from-pink-500 to-sky-500 bg-clip-text !p-2.5 text-6xl font-black text-transparent @xl:self-start @4xl:self-auto @4xl:text-5xl @5xl:text-7xl"
       >
         I'm on
-      </h3>
+      </p>
       <div
         v-if="browser.bundleId"
         class="my-8 flex aspect-square w-2/3 items-center self-center rounded-2xl bg-white @xl:w-1/2 @4xl:w-60 @4xl:self-auto dark:bg-slate-800"
@@ -22,7 +22,7 @@
         />
       </div>
       <p
-        class="self-center bg-gradient-to-tl from-pink-500 to-sky-500 bg-clip-text px-2 py-4 text-6xl font-black text-transparent @xl:self-end @4xl:self-auto @4xl:text-5xl @5xl:text-7xl"
+        class="fix-bg-clip-text-safari self-center bg-gradient-to-tl from-pink-500 to-sky-500 bg-clip-text !p-2.5 text-6xl font-black text-transparent @xl:self-end @4xl:self-auto @4xl:text-5xl @5xl:text-7xl"
       >
         right now.
       </p>
