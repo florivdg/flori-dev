d48c67984577f3f8b2ea3ede05912b952561dc57
Florian van der Galiën <florian.vandergalien@tmt.de>
2023-03-20 10:03:46 +0100

    Added GitHub link and fixed layout for browser

--- src/components/CurrentBrowser.vue
+++ src/components/CurrentBrowser.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="@container">
     <div
-      class="flex -rotate-1 -skew-x-3 flex-col gap-4 rounded-lg border border-slate-300 bg-gray-100 p-4 shadow-xl @4xl:flex-row @4xl:items-center @4xl:justify-center dark:border-slate-700 dark:bg-slate-900"
+      class="flex -rotate-1 -skew-x-3 flex-col rounded-lg border border-slate-300 bg-gray-100 p-4 shadow-xl @4xl:flex-row @4xl:items-center @4xl:justify-center @4xl:gap-4 dark:border-slate-700 dark:bg-slate-900"
     >
       <p
-        class="fix-bg-clip-text-safari self-center bg-gradient-to-tr from-pink-500 to-sky-500 bg-clip-text !p-2.5 text-6xl font-black text-transparent @xl:self-start @4xl:self-auto @4xl:text-5xl @5xl:text-7xl"
+        class="fix-bg-clip-text-safari self-center bg-gradient-to-tr from-pink-500 to-sky-500 bg-clip-text !p-2.5 text-center text-6xl font-black text-transparent @xl:self-start @4xl:self-auto @4xl:text-5xl @5xl:text-7xl"
       >
         I'm on
       </p>
       <div
         v-if="browser.bundleId"
-        class="my-8 flex aspect-square w-2/3 items-center self-center rounded-2xl bg-white @xl:w-1/2 @4xl:w-60 @4xl:self-auto dark:bg-slate-800"
+        class="my-4 flex aspect-square w-2/3 items-center self-center rounded-2xl bg-white @xl:my-8 @xl:w-60 @4xl:self-auto dark:bg-slate-800"
       >
         <img
           :src="`/icons/${browser.bundleId}.svg`"
@@ -22,7 +22,7 @@
         />
       </div>
       <p
-        class="fix-bg-clip-text-safari self-center bg-gradient-to-tl from-pink-500 to-sky-500 bg-clip-text !p-2.5 text-6xl font-black text-transparent @xl:self-end @4xl:self-auto @4xl:text-5xl @5xl:text-7xl"
+        class="fix-bg-clip-text-safari self-center bg-gradient-to-tl from-pink-500 to-sky-500 bg-clip-text !p-2.5 text-center text-6xl font-black text-transparent @xl:self-end @4xl:self-auto @4xl:text-5xl @5xl:text-7xl"
       >
         right now.
       </p>
