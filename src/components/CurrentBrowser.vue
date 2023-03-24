c00e7664a5b693fced54b17052cf84469ae965c5
Florian van der Galiën <hallo@flori.dev>
2023-03-24 10:08:40 +0100

    PoC default browser with web sockets

--- src/components/CurrentBrowser.vue
+++ src/components/CurrentBrowser.vue
@@ -9,12 +9,12 @@
         I'm on
       </p>
       <div
-        v-if="browser.bundleId"
+        v-if="browser"
         class="my-4 flex aspect-square w-2/3 items-center self-center rounded-2xl bg-white @xl:my-8 @xl:w-60 @4xl:self-auto dark:bg-slate-800"
       >
         <img
-          :src="`/icons/${browser.bundleId}.svg`"
-          :alt="browser.bundleId"
+          :src="`/icons/${browser}.svg`"
+          :alt="browser"
           class="aspect-square w-full object-contain p-5 @4xl:p-10"
           loading="lazy"
           width="100"
@@ -31,94 +31,15 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, reactive, watch } from 'vue'
-import { useTimestamp } from '@vueuse/core'
+import { useWebSocket } from '@vueuse/core'
 
 /**
- * Reactive timestamp for live updates.
+ * Reactive websocket for live updates.
  */
-const now = useTimestamp({ offset: 0, interval: 1000 })
-
-/**
- * Reactive data fetching.
- */
-const browser = reactive<{
-  bundleId: string
-  isFetching: boolean
-  error: boolean
-  updatedAt?: Date
-}>({
-  bundleId: '',
-  isFetching: false,
-  error: false,
-  updatedAt: undefined,
-})
-
-/**
- * Fetch the browser ID from Vercel Edge Config.
- */
-const fetchBrowser = async () => {
-  try {
-    /// Set initial fetching state.
-    browser.isFetching = true
-    browser.error = false
-
-    /// Perform fetch.
-    const read = await fetch(
-      `https://edge-config.vercel.com/${
-        import.meta.env.PUBLIC_VERCEL_EDGE_CONFIG_ID
-      }/item/browser?token=${import.meta.env.PUBLIC_VERCEL_EDGE_CONFIG_TOKEN}`,
-    )
-
-    /// Set browser ID.
-    const bundleId = await read.json()
-    browser.bundleId = bundleId
-
-    /// Set updated at.
-    browser.updatedAt = new Date()
-  } catch (error) {
-    browser.error = true
-    console.log(error)
-  } finally {
-    browser.isFetching = false
-  }
-}
-
-/**
- * Fetch browser on mount.
- */
-onMounted(() => {
-  fetchBrowser()
-})
-
-/**
- * Calculate the delta between now and the updated at date.
- */
-const updatedDelta = computed(() => {
-  return ((browser.updatedAt?.getTime() ?? 0) - now.value) / 1000
-})
-
-/**
- * Format updated at date label.
- */
-const updatedAtLabel = computed(() => {
-  const formatter = new Intl.RelativeTimeFormat('en', {
-    numeric: 'auto',
-  })
-
-  return formatter.format(Math.round(updatedDelta.value), 'second')
-})
-
-/**
- * Watch for updated delta changes.
- */
-watch(
-  () => updatedDelta.value,
-  (delta) => {
-    /// Refetch browser if delta is equal or greater than 60 seconds.
-    if (delta <= -60 && !browser.error) {
-      fetchBrowser()
-    }
+const { status, data: browser } = useWebSocket<string>(
+  'ws://localhost:8787/live',
+  {
+    autoReconnect: true,
   },
 )
 </script>
