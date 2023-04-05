7a588e04c2728dc16c489e161f4c3bf9f712af73
Florian van der Galiën <hallo@flori.dev>
2023-04-05 15:24:28 +0200

    Optimized images integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import image from "@astrojs/image";
-
-// https://astro.build/config
-import vue from "@astrojs/vue";
+import image from '@astrojs/image'
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
+import vue from '@astrojs/vue'
+
+// https://astro.build/config
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), vue(), mdx()]
-});
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    vue(),
+    mdx(),
+  ],
+})
