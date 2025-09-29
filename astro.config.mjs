b4c4c666d21f8640243c9480353db73e3c337fb0
Florian van der Galiën <hallo@flori.dev>
2025-09-29 23:32:06 +0200

    chore: update project configuration and dependencies for Netlify
    - Add netlify.toml for Netlify deployment configuration.
    - Update package.json to include @astrojs/netlify and remove Vercel dependencies.
    - Modify SEO component to prepend deploy URL for absolute og:image URLs.
    - Remove SpeedInsights component from Basic layout.
    - Delete vercel.json as the project is transitioning to Netlify.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,6 @@ import vue from '@astrojs/vue'
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
-import vercel from '@astrojs/vercel'
-
-// https://astro.build/config
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
@@ -34,20 +31,9 @@ export default defineConfig({
     sitemap(),
   ],
   output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   vite: {
     define: {
       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
-      'import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID': JSON.stringify(
-        process.env.VERCEL_ANALYTICS_ID,
-      ),
-      'import.meta.env.PUBLIC_VERCEL_URL': JSON.stringify(
-        process.env.VERCEL_URL,
-      ),
     },
     plugins: [tailwindcss()],
   },
